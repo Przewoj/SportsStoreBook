a40aee9c692514edb79e924069f2f55e3599e4a0
Przemek Wojtarowicz <przemyslaw.wojtarowicz@gmail.com>
2018-10-14 19:04:39 +0200

    Branch do rozumienia rzeczy takimi, jakie naprawde sa

---
+++ SportsStore.WebUI/Controllers/GeneralController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class GeneralController : Controller
+    {
+        // GET: General
+        public string General()
+        {
+            return "Hello!";
+        }
+    }
+}
